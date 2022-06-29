f3f26c7e1924bd2ed1f9c5208ca7c8406bfa5e92
Erkin <erkin1399@gmail.com>
2022-06-29 19:04:46 +0600

    Add second elastic project

---
+++ elasticsearch/elasticsearch-second/src/main/java/kg/erkin/elasticsearchsecond/ElasticsearchSecondApplication.java
@@ -0,0 +1,13 @@
+package kg.erkin.elasticsearchsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchSecondApplication.class, args);
+	}
+
+}
---
+++ elasticsearch/elasticsearch-second/src/test/java/kg/erkin/elasticsearchsecond/ElasticsearchSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.erkin.elasticsearchsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
